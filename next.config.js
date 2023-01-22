df2086abc028de2da62b7429fc15b86dc951f0ae
kmariuszk <kiciorm@gmail.com>
2023-01-22 19:05:17 +0100

    first-commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MONGO_URI: "mongodb+srv://mytemplateusername:mytemplatepassword@atlascluster.oewzuja.mongodb.net/?retryWrites=true&w=majority"
+  }
 }
 
 module.exports = nextConfig
