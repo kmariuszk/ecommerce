cd2758e56b647e5caae0483a51b6111e009ad1a6
kmariuszk <kiciorm@gmail.com>
2023-02-04 19:53:08 +0100

    Creating basic homepage (styling)

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,14 +10,14 @@ function index() {
       <section className='homepage--hero-container'>
         <Image className='homepage--hero-image' alt='hero' src={hero}></Image>
         <div className='homepage--hero-text-containger'>
-          <h1 className='homepage--hero-title'>Find your perfect laptop</h1>
+          <h1 className='homepage--hero-title'>Find Your Perfect Laptop</h1>
           <h3 className='homepage--hero-tagline'>Powerful Performance, Portability and Style</h3>
           <button className='homepage--action-button'>Explore Laptops</button>
         </div>
       </section>
       <section className='homepage--features'>
         <Image className='homepage--headphones-image homepage--image' alt='headphones' src={headphones}/>
-        <p className='homepage--features-description'>Our gaming accessories enhance your gaming experience with high-precision controllers, surround sound headphones, and ergonomic mouse pads. Responsive buttons and triggers, clear audio, and comfortable mouse surfaces take your gaming to the next level. Ideal for casual and competitive gamers.</p>
+        <p className='homepage--features-description'>Our gaming accessories offer high-precision controls, surround sound, and ergonomic design for an immersive gaming experience. Ideal for casual and competitive gamers alike.</p>
         <Image className='homepage--keyboard-image homepage--image' alt='keyboard' src={keyboard}></Image>
       </section>
     </>
