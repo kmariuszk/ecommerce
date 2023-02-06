27f9aba4055a3c82a8a27186dfdf6b86e89cdb69
kmariuszk <kiciorm@gmail.com>
2023-02-06 09:25:06 +0100

    Update database

--- models/Product.js
+++ models/Product.js
@@ -1,16 +1,26 @@
-const mongoose = reqiore('mongoose');
+const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
     title: {
         type: String,
         required: [true, 'Please add a title!'],
         unique: true,
-        maxlength: [40, 'Title cannot be more than 40 characters!'],
     },
     description: {
         type: String,
         required: true,
-        maxlength: [200, 'Description cannot be more than 200 characters!']
+    },
+    categories: {
+        type: [String],
+        required: true,
+    },
+    photoLinks: {
+        type: [String],
+        required: true,
+    },
+    price: {
+        type: Number,
+        require: true,
     }
 })
 
---
+++ pages/api/products/index.js
@@ -0,0 +1,36 @@
+import dbConnect from '../../../utils/dbConnect';
+import Product from '../../../models/Product';
+
+dbConnect();
+
+async function index(req, res) {
+    const { method } = req;
+
+    switch (method) {
+        // Get all of the products.
+        case 'GET':
+            try {
+                const products = await Product.find({});
+
+                res.status(200).json({ success: true, data: products });
+            } catch (error) {
+                res.status(400).json({ success: false });
+            }
+            break;
+        // Add a new product.
+        case 'POST':
+            try {
+                const product = await Product.create(req.body);
+
+                res.status(201).json({ success: true, data: product });
+            } catch (error) {
+                res.status(400).json({ success: false });
+            }
+            break;
+        default:
+            res.status(400).json({ success: false });
+            break;
+    }
+};
+
+export default index;
