8584e033e7e8649ba7731a27cd18166e1ecfc382
kmariuszk <kiciorm@gmail.com>
2023-02-10 19:19:54 +0100

    Create about page

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 function about() {
   return (
-    <div>about</div>
+    <div className='about'>
+      <section className='about--introduction'>
+        <h1 className='about-title'>About Tech-Hub</h1>
+        <p>Tech-Hub is an online platform dedicated to providing the best gaming and tech accessories for players. Whether you&apos;re an avid gamer or just someone who appreciates the latest technology, we&apos;ve got you covered. Our mission is to make it easy for you to find the perfect products to enhance your gaming experience.</p>
+      </section>
+      <hr />
+      <section className='about--laptops'>
+        <h2>Gaming Laptops</h2>
+        <p>At Tech-Hub, we understand the importance of having a high-quality gaming laptop. That&apos;s why we&apos;ve carefully selected some of the best gaming laptops on the market to offer you the best gaming experience possible. From budget-friendly options to high-end models, we have something for everyone. Whether you&apos;re a casual gamer or a professional player, you&apos;re sure to find the perfect gaming laptop that fits your needs.</p>
+      </section>
+      <hr />
+      <section className='about--accessories'>
+        <h2>Tech Accessories</h2>
+        <p>In addition to gaming laptops, we also offer a wide range of tech accessories for players. From the latest gaming mice and keyboards, to VR headset and controllers, we have everything you need to complete your gaming setup. We understand the importance of having the right accessories to get the most out of your gaming experience, which is why we only offer high-quality products from trusted brands.</p>
+      </section>
+      <hr />
+      <p>At Tech-Hub, we&apos;re committed to providing you with the best products and service. If you have any questions or need help finding the perfect gaming laptop or tech accessory, our team of experts is here to help. So why wait? Start shopping now and find the perfect products to take your gaming experience to the next level!</p>
+    </div>
   )
 }
 
